7d91184f0aec4232d4cba352f048033660607681
lars <geniuslar@mail.ru>
2024-04-20 15:59:05 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,12 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <GoogleOAuthProvider clientId="145384074994-t5f0j9jqhhsoia1jdhh4e883kk3i3nn7.apps.googleusercontent.com">
-          <App />
-        </GoogleOAuthProvider>
+        <App />
       </PersistGate>
     </Provider>
   </HashRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "react-pizza-v-2.0",
   plugins: [react()],
 })
