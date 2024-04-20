5082bc27143d68b1ce7ea8d2501360addb048819
lars <geniuslar@mail.ru>
2024-04-20 16:09:52 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "react-pizza-v-2.0",
+  base: "/react-pizza-mockapi-v2",
   plugins: [react()],
 })
